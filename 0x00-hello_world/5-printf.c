e5294724e0617a465e39da3c8f131c3ea7f1aa04
AlexNuamah <alexandernauamjr@gmail.com>
2023-08-10 18:16:37 -0700

    My first c program

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
-/* 
- * main - entry point
+
+/* main - entry point
+ * The main function is the starting point of execution for the program.
+ * Return: Always return 0 (success)
  */
 int main(void)
 {
-	printf("with proper grammer, but the outcome is a peice of art,\n");
-		return (0);
+	printf("with proper grammar, but the outcome is a peice of art,\n");
+	return (0);
 }
