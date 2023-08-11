307521e83f4ee0bc3434b290147115091a9a9433
AlexNuamah <alexandernauamjr@gmail.com>
2023-08-11 06:01:18 -0700

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * The main function is the starting point of execution for the program.
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	puts("programming is like building a multilingual puzzle\n");
+	return (0);
+}
