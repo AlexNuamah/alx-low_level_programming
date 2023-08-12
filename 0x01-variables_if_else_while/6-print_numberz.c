417e69686dd7895476a520c50f6dc3db7a1a467b
AlexNuamah <alexandernauamjr@gmail.com>
2023-08-11 17:44:49 -0700

    Printing numbers with putchar

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - The main function is the starting of the execution for the program
+ * Description:
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		putchar(i + '0');
+	}
+	putchar('\n');
+	return (0);
+}
