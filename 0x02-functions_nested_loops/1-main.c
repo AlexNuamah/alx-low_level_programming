1621c5e7bce01d9a0a79236f64059080449b0ea0
AlexNuamah <alexandernauamjr@gmail.com>
2023-08-15 16:17:22 -0700

    Displaying alphabets

---
+++ 0x02-functions_nested_loops/1-main.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * main - The main is the entry point of execution for the program
+ *
+ * Description: It prints the alphabets
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	print_alphabet();
+	return (0);
+}
