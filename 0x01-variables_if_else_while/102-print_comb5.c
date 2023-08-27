006223a28a60c54fb59c06f9d8e482c91eec22d1
AlexNuamah <alexandernauamjr@gmail.com>
2023-08-26 18:23:45 -0700

    Combo 5

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <time.h>
+#include <unistd.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: This is the main function of the program.
+ * It display numbers
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+	int j;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 1; j < 10; j++)
+		{
+			if (i < j)
+			{
+				putchar(i + '0');
+				putchar(j + '0');
+			
+				if (i == 8 && j == 9)
+					continue;
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
