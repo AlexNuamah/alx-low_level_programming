f6608ce049339bfca049f84f627e92011b6e8c68
AlexNuamah <alexandernauamjr@gmail.com>
2023-09-07 17:56:14 -0700

    --no-edit

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,13 +1,15 @@
 #include <stdio.h>
 
 /**
- * main - Prints name of program
+ * main - Display the name for the program. This is
+ * the entry point into the program.
+ *
  *
  * @argc: size of argv array and number of command line arguments
  *
  * @argv: An arrays of sizes argc
  *
- * Returns: 0
+ * Return: The success exit code
  */
 
 int main(int argc __attribute__((unused)), char *argv[])
