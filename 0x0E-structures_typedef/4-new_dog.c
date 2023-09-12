b958ebc058526ac3fb319668f8630bea9ab29fe7
AlexNuamah <alexandernauamjr@gmail.com>
2023-09-11 19:53:52 -0700

    --no-edit

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -44,14 +44,14 @@ dog_t *new_dog(char *name, float age, char *owner)
 		return (NULL);
 	doggo->name = malloc(sizeof(char) * (_strlen(name) + 1));
 	if (doggo->name == NULL)
-	{	
-	    	free(doggo);
+	{
+		free(doggo);
 		return (NULL);
 	}
 	doggo->owner = malloc(sizeof(char) * (_strlen(owner) + 1));
 	if (doggo->owner == NULL)
 	{
-        	free(doggo->name);
+		free(doggo->name);
 		free(doggo);
 		return (NULL);
 	}
