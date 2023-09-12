cb863bdfd19aa154f243f73fcf88e338a40a034a
AlexNuamah <alexandernauamjr@gmail.com>
2023-09-11 21:07:49 -0700

    File name

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,19 @@
+/*
+ * File: 2-main.c
+ * Auth: Alexander Nuamah Jr
+ */
+
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program.
+ *        Prints the name of the current source file.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    printf("%s\n", __FILE__);
+
+    return (0);
+}
