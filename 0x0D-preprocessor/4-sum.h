ffd16cb59f48bf7384cba2b851294241a995db9c
AlexNuamah <alexandernauamjr@gmail.com>
2023-09-11 21:13:52 -0700

    Sum

---
+++ 0x0D-preprocessor/4-sum.h
@@ -0,0 +1,13 @@
+#ifndef SUM_H
+#define SUM_H
+
+/*
+ * File: 4-sum.h
+ * Auth: Alexander Nuamah Jr
+ * Desc: Header file that defines a function-like macro SUM(x, y)
+ *       to compute the sum of the numbers x and y.
+ */
+
+#define SUM(x, y) ((x) + (y))
+
+#endif
