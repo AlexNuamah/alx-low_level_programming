e9fb73572286a2a15309c0618ebef81ef48f3c76
AlexNuamah <alexandernuamahjr@gmail.com>
2023-10-28 06:25:48 +0100

    A library is not a luxury but one of the necessities of life

---
+++ 0x09-static_libraries/3-islower.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _islower - checks for lowercase characters
+ * Description: This function checks for lowercase character
+ * @c: c is the character to be checked
+ *
+ * Return: 1 if character is lowercase, 0 otherwise
+ */
+int _islower(int c)
+{
+if (c >= 'a' && c <= 'z')
+{
+return (1);
+}
+return (0);
+}
---
+++ 0x09-static_libraries/README.md
@@ -0,0 +1,1 @@
+Working with static labrary in c programming.
