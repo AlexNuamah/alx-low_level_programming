4945de49f7c8424da6f95b5e5adc351e6b6dea6b
AlexNuamah <alexandernuamahjr@gmail.com>
2023-11-06 23:56:29 +0000

    Object like marco

---
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -0,0 +1,13 @@
+#ifndef OBJECT_LIKE_MARCO_H
+#define OBJECT_LIKE_MARCO_H
+
+/**
+ * File: 0-object_like_marco_h
+ * Author: Alexander Nuamah Jr
+ * Desc: Header file that defines a macro named
+ *       SIZE as an abbreviation for the token 1024.
+*/
+
+#define SIZE 1024
+
+#endif
