a7778310e78ad22f19a6e8c34b403b7954c5875c
AlexNuamah <alexandernuamahjr@gmail.com>
2023-11-18 22:27:23 +0000

    List length

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * list_len - Return number of elements
+ * @h: Points to struct
+ * Return: Num of elements
+ */
+size_t list_len(const list_t *h)
+{
+size_t counter = 0;
+
+while (h != NULL)
+{
+counter++;
+h = h->next;
+}
+return (counter);
+}
